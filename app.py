e0d45edb58aba517534d44ea3fbd78b94dce7d17
Mikhail Yudin <fagci.nsk@gmail.com>
2021-04-18 22:40:24 +0700

    refactor, async, stats

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from datetime import datetime
 from geopy import distance
 from geopy.point import Point
 from pony.orm import Database, db_session
-from pony.orm.core import ObjectNotFound, PrimaryKey, Required
-from pyrogram import Client, filters, idle
+from pony.orm.core import ObjectNotFound, PrimaryKey, Required, Optional
+from pyrogram import Client, filters
 from pyrogram.types import Location, Message
+from pyrogram.types.user_and_chats.user import User
 import quantumrandom as qr
+
+ADMIN_ID = 202242124
 
 client = Client('session')
 
@@ -16,6 +19,7 @@ db = Database()
 
 class UserSetting(db.Entity):
     id = PrimaryKey(int)
+    username = Optional(str)
     distance = Required(int, default=3000)
     created_at = Required(datetime, default=datetime.utcnow())
     updated_at = Required(datetime, default=datetime.utcnow())
@@ -23,75 +27,61 @@ class UserSetting(db.Entity):
 
 
 @db_session
-def get_user(uid):
+def get_user(user: User) -> UserSetting:
     try:
-        u = UserSetting[uid]
+        u = UserSetting[user.id]
+        if not u.username and user.username:
+            u.username = user.username
     except ObjectNotFound:
-        u = UserSetting(id=uid)
+        u = UserSetting(id=user.id, username=user.username)
     return u
 
 
 @db_session
-def update_points_count(uid):
-    u = UserSetting[uid]
+def update_points_count(user: User):
+    u = UserSetting[user.id]
     u.points_count += 1
     u.updated_at = datetime.utcnow()
 
 
-@client.on_message(filters.location)
-def loc(c: Client, m: Message):
-    m.reply_chat_action('find_location')
-    uid = m.from_user.id
-    u = get_user(uid)
-
-    loc: Location = m.location
-    p = Point(loc.latitude, loc.longitude)
-    try:
-        d = qr.randint(500, u.distance) / 1000
-        angle = qr.randint(0, 360)
-        np = distance.distance(d).destination(p, angle)
-        m.reply_location(np.latitude, np.longitude)
-        c.send_message(m.from_user.id, 'В добрый путь!')
-        update_points_count(uid)
-    except Exception as e:
-        m.reply('Error =(\n%s' % e)
-
-
 @db_session
-def change_distance(m: Message, val: str):
-    d = 0
+def change_distance(user: User, val: str):
+    d = None
+    u = get_user(user)
+    od = u.distance
+
     if val:
         try:
             d = int(val)
-            if d <= 0 or d > 20_000_000:
-                raise ValueError
         except:
-            m.reply('Wrong value')
-            return
+            pass
 
-    u = get_user(m.from_user.id)
-    if d:
-        u.distance = d
-        u.updated_at = datetime.utcnow()
-    else:
-        d = u.distance
-    m.reply('Дистанция: %d м' % d)
+    if d is None or d <= 0 or d > 20_000_000:
+        return od
+
+    u.distance = d
+    u.updated_at = datetime.utcnow()
+
+    return d
 
 
 @client.on_message(filters.regex(r'^\d+'))
-def dist(c: Client, m: Message):
-    change_distance(m, m.text)
+async def dist(c: Client, m: Message):
+    d = change_distance(m.from_user, m.text)
+    await m.reply('Дистанция: %d м' % d)
 
 
 @client.on_message(filters.command('distance'))
-def dist(c: Client, m: Message):
-    change_distance(m, m.text[len('/distance'):])
+async def dist_cmd(c: Client, m: Message):
+    """old variant with distance set"""
+    d = change_distance(m.from_user, m.text[len('/distance'):])
+    await m.reply('Дистанция: %d м' % d)
 
 
 @client.on_message(filters.command(['start', 'help']))
-def dist(c: Client, m: Message):
-    u = get_user(m.from_user.id)
-    m.reply((
+async def dist(c: Client, m: Message):
+    u = get_user(m.from_user)
+    await m.reply((
         '🙋 Добро пожаловать в ваш Фатум!\n\n'
         'Дистанция генерации: %d м.\n\n'
         'Отправьте местоположение '
@@ -100,12 +90,47 @@ def dist(c: Client, m: Message):
     ) % (u.distance))
 
 
+@client.on_message(filters.location)
+async def loc(c: Client, m: Message):
+    await m.reply_chat_action('find_location')
+    user = m.from_user
+    u = get_user(user)
+
+    loc: Location = m.location
+    p = Point(loc.latitude, loc.longitude)
+    try:
+        d = qr.randint(500, u.distance) / 1000
+        angle = qr.randint(0, 360)
+        np = distance.distance(d).destination(p, angle)
+        await m.reply_location(np.latitude, np.longitude)
+        await c.send_message(m.from_user.id, 'В добрый путь!')
+        update_points_count(user)
+    except Exception as e:
+        await m.reply('Error =(\n%s' % e)
+
+
+@db_session
+def get_stats():
+    users = UserSetting.select().order_by(lambda u: -u.points_count).limit(5)
+    return '\n'.join(
+        (
+            '%s %s %s' % (
+                ('@%s' % u.username) if u.username else u.id,
+                u.points_count,
+                u.updated_at.astimezone().strftime('%d.%m.%y %H:%M')
+            )
+        ) for u in users
+    )
+
+
+@ client.on_message(filters.command('stats') & filters.chat(ADMIN_ID))
+async def stats(c: Client, m: Message):
+    s = get_stats()
+    await m.reply_text('Stats:\n\n%s' % s)
+
+
 db.bind(provider='sqlite', filename='db.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
 
 
-client.start()
-
-idle()
-
-client.stop()
+client.run()
